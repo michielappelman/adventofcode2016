6d355efd3f93d9f1e1bc7f2d37bcc7cc1d8f8dee
Michiel Appelman <michiel@appelman.se>
2016-12-05 20:20:25 +0100

    Adds comments for readability and a final print statement.

--- day05/day05.py
+++ day05/day05.py
@@ -16,13 +16,17 @@ def main():
         md5hash = md5()
         md5hash.update(INPUT.encode() + str(i).encode())
         hexdigest = md5hash.hexdigest()
+
         if hexdigest.startswith("00000"):
+            # Tests for Part 1
             if found < 8:
                 password_part1[found] = hexdigest[5]
                 found += 1
+            # Tests for Part 2
             if (hexdigest[5].isdigit() and int(hexdigest[5]) < 8 and
                     password_part2[int(hexdigest[5])] is None):
                 password_part2[int(hexdigest[5])] = hexdigest[6]
+        # Display BLINKENLIGHTS
         if i % 50000 == 0:
             pw1 = [hexdigest[p] if c is None else c for p, c in enumerate(password_part1)]
             pw2 = [hexdigest[p] if c is None else c for p, c in enumerate(password_part2)]
@@ -30,9 +34,9 @@ def main():
             stdout.write(msg)
             stdout.flush()
         i += 1
+    # Final Result
     msg = "PW1: {} -- PW2: {}\r".format("".join(password_part1), "".join(password_part2))
-    stdout.write(msg)
-    stdout.flush()
+    print(msg)
 
 if __name__ == "__main__":
     main()
