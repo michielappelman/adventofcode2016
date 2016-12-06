428217fc879f24e632793f74f4732182b23b0cac
Michiel Appelman <michiel@appelman.se>
2016-12-06 07:23:43 +0100

    Also solves part 2 of day 6

--- day06/day06.py
+++ day06/day06.py
@@ -12,11 +12,13 @@ def main():
         instructions = [[c for c in line.strip()] for line in input_file]
     array = np.array(instructions)
     transposed = array.transpose()
-    message = ""
+    message_part1 = ""
+    message_part2 = ""
     for column in transposed:
-        most_common = Counter("".join(column)).most_common(1)
-        message += most_common[0][0]
-    print(message)
+        counter = Counter("".join(column)).most_common()
+        message_part1 += counter[0][0]
+        message_part2 += counter[-1][0]
+    print(message_part1, message_part2)
 
 if __name__ == "__main__":
     main()
