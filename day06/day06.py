942e187fd00964495758357818f0ba7f87b0fd16
Michiel Appelman <michiel@appelman.se>
2016-12-06 07:52:34 +0100

    TIL about using `zip` to tranpose a matrix.

--- day06/day06.py
+++ day06/day06.py
@@ -3,18 +3,15 @@
 """Day 6: Signals and Noise."""
 
 from collections import Counter
-import numpy as np
 
 INPUT_FILE = "day06_input.txt"
 
 def main():
     with open(INPUT_FILE, 'r') as input_file:
         instructions = [[c for c in line.strip()] for line in input_file]
-    array = np.array(instructions)
-    transposed = array.transpose()
     message_part1 = ""
     message_part2 = ""
-    for column in transposed:
+    for column in zip(*instructions):
         counter = Counter("".join(column)).most_common()
         message_part1 += counter[0][0]
         message_part2 += counter[-1][0]
