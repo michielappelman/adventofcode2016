f70cfb0224c49907298fe810ad970f13077d3474
Michiel Appelman <michiel@appelman.se>
2016-12-28 12:01:00 +0100

    Solves day 20

--- day20/day20.py
+++ day20/day20.py
@@ -3,20 +3,29 @@
 """Day 20: Firewall Rules."""
 
 INPUT_FILE = "day20_input.txt"
+# INPUT_FILE = "day20x_input.txt"
 
 def main():
     with open(INPUT_FILE, 'r') as input_file:
         instructions = [line.strip().split('-') for line in input_file]
     ranges = [(int(low), int(high)) for low, high in instructions]
-    ranges = sorted(ranges, key=lambda x: x[0])
-    ranges.append((4294967296, 4294967296))
+    ranges.sort(key=lambda x: x[0])
 
-    for i, (_, high) in enumerate(ranges):
-        try:
-            if high + 1 < ranges[i + 1][0]:
-                print(high + 1)
-        except:
-            pass
+    ip_address = 0
+    i = 0
+    allowed = []
+    while ip_address < 4294967296:
+        low, high = ranges[i]
+        if ip_address >= low:
+            if ip_address <= high:
+                ip_address = high + 1
+            else:
+                i += 1
+        else:
+            allowed.append(ip_address)
+            ip_address += 1
+    print(allowed[0])
+    print(len(allowed))
 
 if __name__ == "__main__":
     main()
