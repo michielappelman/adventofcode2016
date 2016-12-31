42b4c4c96544c254a77495f09a596d7aec6b3e4a
Michiel Appelman <michiel@appelman.se>
2016-12-31 12:11:00 +0100

    Solves part 2 of day 22

--- day22/day22.py
+++ day22/day22.py
@@ -8,8 +8,94 @@ from itertools import permutations
 
 INPUT_FILE = "day22_input.txt"
 
+Node = namedtuple('Node', ['x', 'y', 'size', 'used', 'avail', 'in_use'])
+
+class Grid:
+    def __init__(self, nodes, goal=(0, 0)):
+        self.nodes = sorted(nodes, key=lambda n: (n.y, n.x))
+        self.width = max(nodes, key=lambda n: n.x).x + 1
+        self.hole = self._find_empty()
+        self.goal = self._coord_to_position(goal)
+
+    def _coord_to_position(self, coords):
+        return coords[0] + coords[1] * self.width
+
+    def _pos_to_coord(self, pos):
+        y = pos // self.width
+        return (pos - y * self.width, y)
+
+    def solved(self, goal):
+        if self.hole == self._coord_to_position(goal):
+            return True
+        return False
+
+    def __str__(self):
+        string_display = ""
+        for i, column in enumerate(self.nodes):
+            if column.used == 0:
+                string_display += "_"
+            elif column.in_use > 90:
+                string_display += "#"
+            else:
+                string_display += "."
+            if i % self.width == self.width - 1 and i != len(self.nodes) - 1:
+                string_display += '\n'
+        return string_display
+
+    def _find_empty(self):
+        return self.nodes.index(min(self.nodes, key=lambda n: n.used))
+
+    def get_next_moves(self):
+        new_poss = []
+        for dest in (self.hole - 1, self.hole + 1): # Left and Right
+            if dest // self.width == self.hole // self.width: # are in same row
+                new_poss.append(dest)
+        for dest in (self.hole - self.width, self.hole + self.width): # Up and Down
+            if 0 <= dest < len(self.nodes):
+                new_poss.append(dest)
+        valid_new_poss = []
+        for pos in new_poss:
+            # if self.nodes[self.hole].avail > self.nodes[pos].used and self.nodes[pos].used < 100:
+            if self.nodes[pos].in_use < 90:
+                valid_new_poss.append(pos)
+        yield from valid_new_poss
+
+    def move(self, destination, goal):
+        nodes = self.nodes[:]
+        nodes[self.hole] = Node(nodes[self.hole].x,
+                                nodes[self.hole].y,
+                                nodes[self.hole].size,
+                                nodes[destination].used,
+                                nodes[self.hole].size - nodes[destination].used,
+                                int((nodes[destination].used / nodes[self.hole].size) * 100))
+        nodes[destination] = Node(nodes[destination].x,
+                                  nodes[destination].y,
+                                  nodes[destination].size,
+                                  0,
+                                  nodes[destination].size,
+                                  0)
+        return Grid(nodes, goal)
+
+def shortest_datapath(start, goal=(0, 0)):
+    seen = set()
+    steps = 0
+
+    next_moves = []
+    next_moves.append((0, start))
+    if start.solved(goal):
+        return next_moves.pop()
+
+    while next_moves:
+        next_move = next_moves.pop(0)
+        steps, grid = next_move
+        if grid.hole not in seen:
+            if grid.solved(goal):
+                return steps
+            for destination in grid.get_next_moves():
+                next_moves.append((steps + 1, grid.move(destination, goal)))
+        seen.add(grid.hole)
+
 def main():
-    Node = namedtuple('Node', ['x', 'y', 'size', 'used', 'avail', 'in_use'])
     nodes = []
 
     with open(INPUT_FILE, 'r') as input_file:
@@ -22,7 +108,11 @@ def main():
     for pair in permutations(nodes, 2):
         if pair[0].used != 0 and pair[0].used < pair[1].avail:
             viable_pair_count += 1
-    print(viable_pair_count)
+    print("Star 1:", viable_pair_count)
+
+    grid = Grid(nodes)
+    steps = shortest_datapath(start=grid, goal=(grid.width - 1, 0))
+    print("Star 2:", steps + (5 * (grid.width - 2)))
 
 if __name__ == "__main__":
     main()
